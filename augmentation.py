ae914a87e6dcd503727e6c3d811edebc48fcb74c
Lam <thaiminhlam1177@gmail.com>
2025-08-13 20:39:46 +0700

    update code augmentation

--- augmentation.py
+++ augmentation.py
@@ -8,13 +8,15 @@ import faiss
 import spacy
 import itertools
 from vncorenlp import VnCoreNLP
+from tqdm import tqdm
 
 res = faiss.StandardGpuResources()
 MODEL_NAME = "bert-base-multilingual-cased"
+OUTPUT_FILE = "augmentation"
 NLP = spacy.load("en_core_web_sm")
 
 class Dictionary_base_Augmentation(nn.Module):
-    def __init__(self, model_name: str, batch_size: int, threshold: float, align_layer: int, topN: int):
+    def __init__(self, file_out: str, model_name: str, batch_size: int, threshold: float, align_layer: int, topN: int):
         super().__init__()
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -24,6 +26,8 @@ class Dictionary_base_Augmentation(nn.Module):
         self.threshold = threshold
         self.align_layer = align_layer
         self.topN = topN
+        self.f_en_out = open(f"/content/drive/MyDrive/[VLSP] MT in Medical Domain/data/{file_out}.en.txt", 'w', encoding='utf-8')
+        self.f_vi_out = open(f"/content/drive/MyDrive/[VLSP] MT in Medical Domain/data/{file_out}.vi.txt", 'w', encoding='utf-8')
 
     def get_batch_embeddings(self, sentences: list[str]) -> torch.Tensor:
         embeddings = []
@@ -42,7 +46,7 @@ class Dictionary_base_Augmentation(nn.Module):
         index.add(src_ood_parallel_corpus_embeddings)
         faiss.normalize_L2(src_ood_parallel_corpus_embeddings) # Normalize L2
         faiss.normalize_L2(src_ind_dic_embeddings) # Normalize L2
-        _, indices = index.search(src_ind_dic_embeddings, k=self.topN)
+        _, indices = gpu_index_flat.search(src_ind_dic_embeddings, k=self.topN)
         return indices.tolist()
 
     def extract_en_noun_phrases(self, sentence: str) -> list[str]:
@@ -110,7 +114,7 @@ class Dictionary_base_Augmentation(nn.Module):
         return mapping_align_words
 
     def substitute(self, ood_sentence: str, phrase_max_ood: str, phrase_ind: str) -> str:
-        return ood_sentence.replace(phrase_max_ood, phrase_ind)
+        return ood_sentence.replace(phrase_max_ood, phrase_ind).strip()
 
     def forward(self, ood_parallel_corpus, ind_dic):
         src_odd_parallel_corpus, tgt_odd_parallel_corpus = [], []
@@ -127,10 +131,8 @@ class Dictionary_base_Augmentation(nn.Module):
         src_ind_dic_embeddings = self.get_batch_embeddings(src_ind_dic)
         src_ood_topN_sen_corpus_indices = self.get_topN_src_sentences(src_ood_parallel_corpus_embeddings, src_ind_dic_embeddings)
         assert len(src_ood_topN_sen_corpus_indices) == src_ind_dic_embeddings.shape[0], "Not Suitable Size"
-        Gc = []
-        for i in range(src_ind_dic_embeddings.shape[0]):
+        for i in tqdm(range(src_ind_dic_embeddings.shape[0]), desc='Similarity to src_dic'):
             src_embedding = src_ind_dic_embeddings[i].reshape(1, -1)
-            ic(src_embedding.dtype)
             for id_sen in src_ood_topN_sen_corpus_indices[i]:
                 phrases_Sk = self.extract_en_noun_phrases(src_odd_parallel_corpus[id_sen])
                 phrases_Tk = self.extract_vi_noun_phraes(tgt_odd_parallel_corpus[id_sen])
@@ -146,8 +148,19 @@ class Dictionary_base_Augmentation(nn.Module):
                 phrase_max_Sk = phrases_Sk[max_sim_index[0].item()]
                 phrase_max_Tk = mapping_align_words[phrase_max_Sk]
 
-                Gc.append(
-                    (self.substitute(src_odd_parallel_corpus[id_sen], phrase_max_Sk, src_ind_dic[i]),
-                    self.substitute(tgt_odd_parallel_corpus[id_sen], phrase_max_Tk, tgt_ind_dic[i]))
-                )
-        return Gc
+                aug_src_sen = self.substitute(src_odd_parallel_corpus[id_sen], phrase_max_Sk, src_ind_dic[i])
+                aug_tgt_sen = self.substitute(tgt_odd_parallel_corpus[id_sen], phrase_max_Tk, tgt_ind_dic[i])
+                self.f_en_out.write(f"{aug_src_sen}\n")
+                self.f_vi_out.write(f"{aug_tgt_sen}\n")
+
+
+if __name__ == '__main__':
+    ind_dic_json = json.load(open("/content/drive/MyDrive/[VLSP] MT in Medical Domain/data/Final/Medical_Dictionary.json", 'r', encoding='utf-8'))
+    ind_dic = [(en_term, vi_term) for en_term, vi_terms in ind_dic_json.items() for vi_term in vi_terms]
+    en_ood = open("/content/drive/MyDrive/[VLSP] MT in Medical Domain/data/Final/train.en.txt", 'r', encoding='utf-8')
+    vi_ood = open("/content/drive/MyDrive/[VLSP] MT in Medical Domain/data/Final/train.vi.txt", 'r', encoding='utf-8')
+    ood_parallel_corpus = []
+    for en_sen, vi_sen in zip(en_ood, vi_ood):
+        ood_parallel_corpus.append((en_sen, vi_sen))
+    augmentation_model = Dictionary_base_Augmentation(OUTPUT_FILE, MODEL_NAME, batch_size=256, threshold=1e-3, align_layer=8, topN=5)
+    augmentation_model(ood_parallel_corpus, ind_dic)
